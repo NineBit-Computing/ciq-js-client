a0072ab2c9e3e5c2b7b310c3cf235ef05e9bc101
Rajeev J <rajeev.jamaiyar@ninebit.in>
2025-06-26 17:53:30 +0530

    fix: CI failing error

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
     ...tsJestTransformCfg,
   },
-  testMatch: ["**/tests/**/*.test.ts"],
+  testMatch: ['**/tests/**/*.test.ts'],
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
-  coverageReporters: ["text", "lcov"],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  coverageReporters: ['text', 'lcov'],
 };
